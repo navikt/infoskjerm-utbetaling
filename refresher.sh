c296f7f7e664f2bdfa3e26ea7b085b464d7b512e
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2025-03-31 14:36:21 +0200

    bruker nytt reponavn

--- refresher.sh
+++ refresher.sh
@@ -2,5 +2,5 @@
 echo "Refresherl for tabs"
 
 sleep 5
-lxterminal -t refresher -e 'sleep 2; cd /home/pi/Desktop/infoskjerm; git pull; sleep 2; 
+lxterminal -t refresher -e 'sleep 2; cd /home/pi/Desktop/infoskjerm-karusell; git pull; sleep 2; 
 . .venv/bin/activate; python3 infoskjerm_browser.py; exec bash'
