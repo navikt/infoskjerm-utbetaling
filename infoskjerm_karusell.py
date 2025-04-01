97ef946284131ed39f6233c95babf7156afef247
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2025-04-01 12:02:38 +0200

    flytter sjekk på internettilkobling til bash script

--- infoskjerm_karusell.py
+++ infoskjerm_karusell.py
@@ -10,28 +10,19 @@ mac = False
 cmd = "command" if mac else "ctrl"
 tid_i_hver_fane = 60  # seconds
 
-# sjekker om det er internettforbindelse, som kan ta litt tid
-while True:
-    try:
-        res = socket.getaddrinfo("data.ansatt.nav.no", 80)
-        print("Internett er tilgjengelig :party:")
-        break
-    except:
-        print("Ingen internett, prøver igjen om 5 sekunder")
-        time.sleep(5)
 
 # åpner google.com for å slippe "vil du gjenåpne faner" melding
 webbrowser.open("https://www.google.com")
 time.sleep(4)
 with pyautogui.hold(cmd):
     pyautogui.press("w")
-time.sleep(2)
+time.sleep(1)
 
 
-# åpner en nav-side som lukkes etter 30 sekunder, som løser probler med innlogging
+# åpner en nav-side som lukkes etter 30 sekunder, som løser redirect ved AD-innlogging
 temp_nav_side = "https://data.ansatt.nav.no/quarto/0b700511-f50c-4059-b519-32fb19637bae/bemanning.html"
 webbrowser.open(temp_nav_side)
-time.sleep(30)
+time.sleep(10)
 
 # åpne alle faner og lukker temp_nav_side
 for tab in nettsider:
--- karusell.sh
+++ karusell.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-echo "Karusell for infoskjerm"
+echo "Starter opp karusell for infoskjerm"
 
+# Sjekker om internett er tilgjengelig
+while true; do
+    if ping -c 1 -q google.com &>/dev/null; then
+        echo "Internett er tilgjengelig :party:"
+        break
+    else
+        echo "Ingen internett, prøver igjen om 5 sekunder"
+        sleep 5
+    fi
+done
+
+# Starter infoskjerm-karusell 
 sleep 5
-lxterminal -t karusell -e 'sleep 2; cd /home/pi/Desktop/infoskjerm-karusell; git pull; sleep 2; 
+lxterminal -t Karusellen -e 'sleep 2; cd /home/pi/Desktop/infoskjerm-karusell; git pull; sleep 2; 
 . .venv/bin/activate; python3 infoskjerm_karusell.py; exec bash'
