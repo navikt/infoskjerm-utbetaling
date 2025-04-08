cbcdbde6878539a523914abcf772df8a7ba14f61
Brynjar <141220683+navbrynjar@users.noreply.github.com>
2025-04-08 15:11:50 +0200

    Update nettsidelenker.py

--- nettsidelenker.py
+++ nettsidelenker.py
@@ -5,19 +5,28 @@ nettsider = [
 
     # Quarto - Månedlige uføremottakere (offentlig statistikk)
     'https://data.ansatt.nav.no/quarto/d1e4cefc-2658-4519-a8c0-0f29db301d9d/uforemottakere.html#plott-og-tabell',
-    'https://data.ansatt.nav.no/quarto/d1e4cefc-2658-4519-a8c0-0f29db301d9d/uforemottakere.html#tilgang-og-avgang',
+    #'https://data.ansatt.nav.no/quarto/d1e4cefc-2658-4519-a8c0-0f29db301d9d/uforemottakere.html#tilgang-og-avgang',
 
     # Quarto - bemanning
     'https://data.ansatt.nav.no/quarto/0b700511-f50c-4059-b519-32fb19637bae/bemanning.html#pensjonsomr%C3%A5det',
     'https://data.ansatt.nav.no/quarto/0b700511-f50c-4059-b519-32fb19637bae/bemanning.html#poene-2d-plott',
 
     # Metabase
-    'https://metabase.ansatt.nav.no/public/dashboard/5f8908a8-1448-4993-be8e-1ddb0dbc1068',
-    'https://metabase.ansatt.nav.no/public/dashboard/72aa5771-9693-4369-b270-bd3c28798523?e-nr=E261&spesifikk_m%25C3%25A5ned=&tab=339-status-per-team&valgt_team=Pesys-Uf%C3%B8re',
+    # Metabase - offentlig statistikk
+    'https://metabase.ansatt.nav.no/public/dashboard/5f8908a8-1448-4993-be8e-1ddb0dbc1068?tab=379-mottakere',
+    'https://metabase.ansatt.nav.no/public/dashboard/5f8908a8-1448-4993-be8e-1ddb0dbc1068?tab=378-oversikt',
+
+    # Metabase - inntektsendringer og autobrev
+    'https://metabase.ansatt.nav.no/public/dashboard/fe4fae92-28b3-4ccc-9376-1ed38fff7d0a?%25C3%25A5r=2025&brevtype=&m%25C3%25A5ned=&tab=381-infoskjerm1',
     'https://metabase.ansatt.nav.no/public/dashboard/fe4fae92-28b3-4ccc-9376-1ed38fff7d0a?%25C3%25A5r=2025&brevtype=&m%25C3%25A5ned=',
     'https://metabase.ansatt.nav.no/public/dashboard/fe4fae92-28b3-4ccc-9376-1ed38fff7d0a?%25C3%25A5r=2025&%25C3%25A5r=2024&brevtype=&m%25C3%25A5ned=',
-    'https://metabase.ansatt.nav.no/public/dashboard/993bc3ec-5841-4f33-a8b4-c4ad1b731072'
-
-    # Hvor er damene?
-    'https://data.ansatt.nav.no/quarto/7ea943c9-ae07-4d75-9b65-d775c05230dc/index.html#kvinneandel-i-tech',
+    
+    # Metabase - Uførebrev - samling av målinger (gammel)
+    # 'https://metabase.ansatt.nav.no/public/dashboard/993bc3ec-5841-4f33-a8b4-c4ad1b731072'
+    
+    # Metabase - etterlevelse
+    # 'https://metabase.ansatt.nav.no/public/dashboard/72aa5771-9693-4369-b270-bd3c28798523?e-nr=E261&spesifikk_m%25C3%25A5ned=&tab=339-status-per-team&valgt_team=Pesys-Uf%C3%B8re',
+    
+    # Quarto - Hvor er damene?
+    #'https://data.ansatt.nav.no/quarto/7ea943c9-ae07-4d75-9b65-d775c05230dc/index.html#kvinneandel-i-tech',
 ]
