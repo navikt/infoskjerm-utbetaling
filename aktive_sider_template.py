22e782d023450bdab2b9f7193bdf32bb509b1ccb
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2025-05-07 14:01:39 +0200

    template for aktive_sider.py

---
+++ aktive_sider_template.py
@@ -0,0 +1,9 @@
+# dette er et template, som blir overskrevet av git (så endringer lokalt er ikke lurt)
+
+# denne er ignorert av git, slik at den kan endres på hvert enkelt Rpi
+# pek kun på navnet på listene i nettsidelenker.py som skal vises på skjermen
+
+vis_følgende_lister_med_sider = [
+    "standardsider",
+    "uføre",
+]
